f28fc95c6e17785fd7c4a777323b0f69259d8a98
Fernando Mojica <fernmarmojica@gmail.com>
2022-11-14 09:24:08 -0600

    Building changes

--- dist/app.js
+++ dist/app.js
@@ -1,3 +1,25 @@
-"use strict";
-console.log("jhello world");
+import express from 'express';
+import mysql from 'mysql';
+import dotenv from 'dotenv';
+dotenv.config();
+const app = express();
+const connectionString = process.env.DATABASE_URL || '';
+const connection = mysql.createConnection(connectionString);
+connection.connect();
+app.get('/api/drivers', (req, res) => {
+    const query = 'Select * from Characters;';
+    connection.query(query, (err, rows) => {
+        if (err)
+            throw err;
+        const retVal = {
+            data: rows.length > 0 ? rows[0] : null,
+            message: rows.length === 0 ? 'No Records Found' : 'Success'
+        };
+        return res.send(retVal);
+    });
+});
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log('App is running');
+});
 //# sourceMappingURL=app.js.map
--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,19 @@
 import express, {Request, Response} from 'express';
 import mysql from 'mysql';
+import dotenv from 'dotenv';
 
+dotenv.config();
 const app = express();
 
 const connectionString = process.env.DATABASE_URL || '';
 const connection = mysql.createConnection(connectionString);
 connection.connect();
 
-app.get('/api/characters', (req: Request, res: Response) => {
+app.get('/api/drivers', (req: Request, res: Response) => {
   const query = 'Select * from Characters;';
   connection.query(query, (err, rows) => {
+    if (err) throw err;
+
     const retVal = {
       data: rows.length > 0 ? rows[0] : null,
       message: rows.length === 0 ? 'No Records Found' : 'Success'
@@ -17,12 +21,6 @@ app.get('/api/characters', (req: Request, res: Response) => {
     return res.send(retVal);
   })
 });
-
-app.get('/api/characters/:id', (req: Request, res: Response) => {
-  const id = req.params.id;
-  res.send('It works for id: ' + id);
-});
-
 
 const port = process.env.PORT || 3000;
 app.listen(port, ()=> {
